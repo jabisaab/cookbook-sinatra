1491a70e425d9b6a75aaa164e29dbf911840041c
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-01-31 14:50:59 +0000

    completed 101 Sinatra

--- app.rb
+++ app.rb
@@ -2,11 +2,24 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+
 get '/' do
-  'Hello first Sinatra post !'
+  erb :index
+end
+
+
+get '/about' do
+  erb :about
+end
+
+get '/' do
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
 end
